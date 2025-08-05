001305d15713809da23d29768f813aa477510684
devlo90 <devlohani14251425@gmail.com>
2025-08-05 20:50:03 +0530

    trcp

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,23 @@
+import {getQueryClient,trpc} from "@/trpc/server";
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+import { text } from "stream/consumers";
+import { client as Client } from "./client";
+import { Suspense } from "react";
 
-const page = () => {
 
+const page =async() => {
+
+  const queryClient=getQueryClient();
+  void queryClient.prefetchQuery(trpc.createAI.queryOptions({text:"Dev pre"}))
 
   return (
-    <div>hello</div>
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Client/>
+      </Suspense>
+    </HydrationBoundary>
+      
+   
   )
 }
 
---
+++ src/trpc/routers/_app.ts
@@ -0,0 +1,19 @@
+import { z } from 'zod';
+import { baseProcedure, createTRPCRouter } from '../init';
+
+export const appRouter = createTRPCRouter({
+  createAI: baseProcedure
+    .input(
+      z.object({
+        text: z.string(),
+      }),
+    )
+    .query(({input}) => {
+      return {
+        greeting: `hello ${input.text}`,
+      };
+    }),
+});
+
+// export type definition of API
+export type AppRouter = typeof appRouter;
